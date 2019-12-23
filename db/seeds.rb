70b3a11ccf173be06537a5ac5e49c229caddb25e
slzander <staceyzander@gmail.com>
2019-12-23 11:06:15 -0700

    adds to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,41 @@ onepenguin = Number.create(
     count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/04/one-penguin-180x255.jpg' 
 )
 
+onetoothbrush = Number.create(
+    name: 'one',
+    num: 1,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149951_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/04/1-toothbrush-with-paste-180x255.jpg' 
+)
+
+oneflower = Number.create(
+    name: 'one',
+    num: 1,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149951_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/04/1-smiling-flower-180x255.jpg' 
+)
+
+onebutterfly = Number.create(
+    name: 'one',
+    num: 1,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149951_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/04/1-yellow-butterfly-180x255.jpg' 
+)
+
+onesweater = Number.create(
+    name: 'one',
+    num: 1,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149951_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/04/1-sweater-180x255.jpg' 
+)
+
+oneumbrella = Number.create(
+    name: 'one',
+    num: 1,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149951_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/04/1-umbrella-180x255.jpg' 
+)
+
 twoowl = Number.create(
     name: 'two',
     num: 2,
@@ -27,6 +62,41 @@ twopencil = Number.create(
     num: 2,
     number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149952_1280.png',
     count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/07/2-pencils-180x255.jpg' 
+)
+
+twoballoon = Number.create(
+    name: 'two',
+    num: 2,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149952_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/07/2-balloons-180x255.jpg' 
+)
+
+twolock = Number.create(
+    name: 'two',
+    num: 2,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149952_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/07/2-locks-180x255.jpg' 
+)
+
+tworose = Number.create(
+    name: 'two',
+    num: 2,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149952_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/07/2-roses-180x255.jpg' 
+)
+
+twocandle = Number.create(
+    name: 'two',
+    num: 2,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149952_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/07/2-candles-180x255.jpg' 
+)
+
+twopin = Number.create(
+    name: 'two',
+    num: 2,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149952_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/07/2-safety-pins-180x255.jpg' 
 )
 
 threeicecream = Number.create(
@@ -43,17 +113,382 @@ threefrog = Number.create(
     count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/04/3-frogs-180x255.jpg' 
 )
 
-fourowl = Number.create(
-    name: 'four',
-    num: 4,
-    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
-    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/07/4-objects-5-180x255.jpg' 
+threehat = Number.create(
+    name: 'three',
+    num: 3,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149953_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/04/3-hats-180x255.jpg' 
 )
 
-fourmushroom = Number.create(
+threeladybug = Number.create(
+    name: 'three',
+    num: 3,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149953_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/04/3-insects-180x255.jpg' 
+)
+
+threemug = Number.create(
+    name: 'three',
+    num: 3,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149953_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/04/3-mugs-180x255.jpg' 
+)
+
+threemushroom = Number.create(
+    name: 'three',
+    num: 3,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149953_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/04/3-mushroom-houses-180x255.jpg' 
+)
+
+threegiraffe = Number.create(
+    name: 'three',
+    num: 3,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149953_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/04/3-giraffes-180x255.jpg' 
+)
+
+fourbee = Number.create(
     name: 'four',
     num: 4,
     number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
-    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/07/4-objects-1-180x255.jpg' 
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/04/4-bees-180x255.jpg' 
+)
+
+fourbandaid = Number.create(
+    name: 'four',
+    num: 4,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/04/4-band-aids-180x255.jpg' 
+)
+
+fourwatch = Number.create(
+    name: 'four',
+    num: 4,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/04/4-black-wrist-watches-180x255.jpg' 
+)
+
+fourcherry = Number.create(
+    name: 'four',
+    num: 4,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/04/4-cherries-180x255.jpg' 
+)
+
+fourbinoculars = Number.create(
+    name: 'four',
+    num: 4,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/04/4-binoculars-180x255.jpg' 
+)
+
+fourant = Number.create(
+    name: 'four',
+    num: 4,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/04/4-ants-180x255.jpg' 
+)
+
+fourbird = Number.create(
+    name: 'four',
+    num: 4,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/04/4-birds-180x255.jpg' 
+)
+
+fivecake = Number.create(
+    name: 'five',
+    num: 5,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/07/cakes-180x255.jpg' 
+)
+
+fivefish = Number.create(
+    name: 'five',
+    num: 5,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/07/fish2-180x255.jpg' 
+)
+
+fivehippo = Number.create(
+    name: 'five',
+    num: 5,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/07/hippopotamus1-180x255.jpg' 
+)
+
+fivebow = Number.create(
+    name: 'five',
+    num: 5,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/07/bows-180x255.jpg' 
+)
+
+fivebee = Number.create(
+    name: 'five',
+    num: 5,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/07/bees-180x255.jpg' 
+)
+
+fivecandle = Number.create(
+    name: 'five',
+    num: 5,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/07/candles-180x255.jpg' 
+)
+
+fivebird = Number.create(
+    name: 'five',
+    num: 5,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/07/birds-180x255.jpg' 
+)
+
+sixglasses = Number.create(
+    name: 'six',
+    num: 6,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/6-pink-glasses-180x255.jpg' 
+)
+
+sixshirt = Number.create(
+    name: 'six',
+    num: 6,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/6-shirts-180x255.jpg' 
+)
+
+sixspoon = Number.create(
+    name: 'six',
+    num: 6,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/6-spoons-180x255.jpg' 
+)
+
+sixclock = Number.create(
+    name: 'six',
+    num: 6,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/6-wall-clocks-180x255.jpg' 
+)
+
+sixelephant = Number.create(
+    name: 'six',
+    num: 6,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/6-tiny-elephants-180x255.jpg' 
+)
+
+sixsoap = Number.create(
+    name: 'six',
+    num: 6,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/6-soap-dispensers-with-soap-180x255.jpg' 
+)
+
+sixbucket = Number.create(
+    name: 'six',
+    num: 6,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/6-pink-buckets-180x255.jpg' 
+)
+
+sevenstrawberry = Number.create(
+    name: 'seven',
+    num: 7,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/7-strawberries-180x255.jpg' 
+)
+
+sevenphone = Number.create(
+    name: 'seven',
+    num: 7,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/7-telephones-180x255.jpg' 
+)
+
+sevenhat = Number.create(
+    name: 'seven',
+    num: 7,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/7-woolen-caps-180x255.jpg' 
+)
+
+sevenpencil = Number.create(
+    name: 'seven',
+    num: 7,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/7-yellow-pencils-180x255.jpg' 
+)
+
+seventomato = Number.create(
+    name: 'seven',
+    num: 7,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/7-tomatoes-180x255.jpg' 
+)
+
+sevenflower = Number.create(
+    name: 'seven',
+    num: 7,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/7-smiling-flowers-180x255.jpg' 
+)
+
+sevenrose = Number.create(
+    name: 'seven',
+    num: 7,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/7-yellow-roses-180x255.jpg' 
+)
+
+eightgift = Number.create(
+    name: 'eight',
+    num: 8,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/8-gifts-180x255.jpg' 
+)
+eightfly = Number.create(
+    name: 'eight',
+    num: 8,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/8-house-flies-180x255.jpg' 
+)
+eightcrayon = Number.create(
+    name: 'eight',
+    num: 8,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/8-pencil-colors-180x255.jpg' 
+)
+eightpineapple = Number.create(
+    name: 'eight',
+    num: 8,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/8-pineapples-180x255.jpg' 
+)
+eightketchup = Number.create(
+    name: 'eight',
+    num: 8,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/8-ketchup-bottles-180x255.jpg' 
+)
+eighttie = Number.create(
+    name: 'eight',
+    num: 8,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/8-neck-ties-180x255.jpg' 
+)
+eightmoney = Number.create(
+    name: 'eight',
+    num: 8,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/8-money-bags-180x255.jpg' 
+)
+
+nineguitar = Number.create(
+    name: 'nine',
+    num: 9,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/9-guitars-180x255.jpg' 
+)
+
+ninechick = Number.create(
+    name: 'nine',
+    num: 9,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/9-ducklings-180x255.jpg' 
+)
+
+ninetree = Number.create(
+    name: 'nine',
+    num: 9,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/9-christmas-trees-180x255.jpg' 
+)
+
+ninealligator = Number.create(
+    name: 'nine',
+    num: 9,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/9-crocodiles-180x255.jpg' 
+)
+
+ninedart = Number.create(
+    name: 'nine',
+    num: 9,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/9-darts-180x255.jpg' 
+)
+
+nineduck = Number.create(
+    name: 'nine',
+    num: 9,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/9-ducks-180x255.jpg' 
+)
+
+ninefly = Number.create(
+    name: 'nine',
+    num: 9,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/9-house-flies-180x255.jpg' 
+)
+
+nineicecream = Number.create(
+    name: 'nine',
+    num: 9,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/9-ice-cream-cones-180x255.jpg' 
+)
+
+tenbutterfly = Number.create(
+    name: 'ten',
+    num: 10,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/10-yellow-butterflies-180x255.jpg' 
+)
+
+tentrash = Number.create(
+    name: 'ten',
+    num: 10,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/10-trash-bins-180x255.jpg' 
+)
+
+tenflashlight = Number.create(
+    name: 'ten',
+    num: 10,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/10-torches-180x255.jpg' 
+)
+
+tenstar = Number.create(
+    name: 'ten',
+    num: 10,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/10-stars-180x255.jpg' 
+)
+
+tenflower = Number.create(
+    name: 'ten',
+    num: 10,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/10-smiley-flowers-180x255.jpg' 
+)
+
+tenspoon = Number.create(
+    name: 'ten',
+    num: 10,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/10-spoons-180x255.jpg' 
+)
+
+tenwatch = Number.create(
+    name: 'ten',
+    num: 10,
+    number_image: 'https://cdn.pixabay.com/photo/2013/07/12/15/30/counting-149954_1280.png',
+    count_image: 'http://www.mathsdiary.com/wp-content/uploads/2015/05/10-wrist-watches-180x255.jpg' 
 )
 
