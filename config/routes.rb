6fb365c88b5ab0e1027c901cd0e549d9a586c4c1
slzander <staceyzander@gmail.com>
2020-01-07 18:30:17 -0700

    comments out get route for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :favorites
   resources :numbers
 
-  get '/', to 'numbers#index'
+  # get '/', to 'numbers#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
